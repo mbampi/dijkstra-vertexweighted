afd836e62bc29a2a877a2af3ec300b026442e069
ostjen <leonardoostjen@gmail.com>
2019-06-29 19:35:13 -0300

    input edges added to graph class

--- graph_v2.py
+++ graph_v2.py
@@ -8,7 +8,7 @@ class Graph(object):
             an empty dictionary will be used
         """
         if graph_dict == None:
-            graph_dict = OrderedDict
+            graph_dict = {}     
         self.__graph_dict = graph_dict
 
     
@@ -23,31 +23,19 @@ class Graph(object):
         if vertex1 in self.__graph_dict:
             self.__graph_dict[vertex1].append((vertex2,weight))
         else:
-            self.__graph_dict[vertex1] = [(vertex2,weight)]
+            self.__graph_dict[vertex1] = (vertex2,weight)
     
-    def __generate_edges(self):
-        """ A static method generating the edges of the 
-            graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
-        """
-        edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
-
-
 
 
     def get_vertices(self):
         """ returns the vertices of a graph """
         return list(self.__graph_dict.keys())
 
-    def get_edges(self):
-        """ returns the edges of a graph """
-        return self.__generate_edges()
+    def keys_values(self):
+        return self.__graph_dict.keys(),self.__graph_dict.values()
+
+        
+         
 
 
 
--- main.py
+++ main.py
@@ -13,20 +13,48 @@ def create_vertices(graph,weights,n):
     for i in range(0,n):
         graph.add_vertex(i,weights[i])
     
+
 def edge2graph(graph,n):
-    for i in range(0,n):
-        pass
+    vertices = graph.get_vertices()    
+    for vertex in vertices:
+        edge_weight = get_line_input()
+        check_size(edge_weight,n)
+        pos = 0                                            #position counter 
+        for weight in edge_weight:
+            if weight != -1:
+                graph.add_edge([vertex,vertices[pos]],weight)
+            pos = pos + 1
+    
+    keys,values = graph.keys_values()
+    print(keys)
+    print('\n\n')
+    values = list(values)
+    print("value[0] = " + str(values[0]))
+    print('\n')
+    print("value[1] = " + str(values[1]))
+    print('\n')
+    print("value[2] = " + str(values[2]))
+    print('\n')
+    print("value[3] = " + str(values[3]))
+    print('\n')
+    print("value[4] = " + str(values[4]))
+    
+    
+
+    
 
 
 
 
-def create_graph(n):
+
+def input2graph(n):
     graph = Graph()
     v_weights = get_line_input()
     check_size(v_weights,n)
     create_vertices(graph,v_weights,n)
-    print(graph.get_vertices())
-    #edge2graph(graph,n)
+    edge2graph(graph,n)
+
+    return graph
 
  
 
@@ -34,7 +62,8 @@ def create_graph(n):
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
     print("Problema dos Caminhos Mínimos de Fonte única com Pesos nos Vértices e nas Arestas")
-
     n = int(input())
     target_vertices = get_line_input()    
-    create_graph(n)
+    graph = input2graph(n)
+    vertices = graph.get_vertices()
+    
